35143eece7b55d8287b4d6887b600230e9473427
ephraimphrasee <ephraimphrase@gmail.com>
2021-12-11 18:53:50 +0100

    commit push

--- projects/views.py
+++ projects/views.py
@@ -14,7 +14,7 @@ def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single-project.html', {'project': projectObj})
 
-@login_required
+@login_required(login_url='login')
 def createProject(request):
     form = ProjectForm()
 
@@ -27,6 +27,7 @@ def createProject(request):
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
 
+@login_required(login_url='login')
 def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance= project)
@@ -40,6 +41,7 @@ def updateProject(request, pk):
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
 
+@login_required(login_url='login')
 def deleteProject(request, pk):
     project = Project.objects.get(id=pk)
     if request.method == 'POST':
--- users/views.py
+++ users/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 
 # Create your views here.
 def loginUser(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('profiles')
 
@@ -17,7 +20,7 @@ def loginUser(request):
         try:
             user = User.objects.get(username=username)
         except:
-            print('User does not exist')
+            messages.error(request, 'User does not exist')
 
         user = authenticate(request, username=username, password=password)
 
@@ -25,12 +28,34 @@ def loginUser(request):
             login(request, user)
             return redirect('profiles')
         else:
-            print('Username or password is incorrect')
+            messages.error(request, 'Username or password is incorrect')
     return render(request, 'users/login_register.html')
 
 def logoutUser(request):
     logout(request)
+    messages.error(request, 'User was logged out')
     return redirect('login')
+
+def registerUser(request):
+    page = 'register'
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created!')
+            
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, 'An error has occurred during registration')
+
+    context = {'page':page, 'form':form}
+    return render(request, 'users/login_register.html', context)
 
 def profiles(request):
     profiles = Profile.objects.all()
